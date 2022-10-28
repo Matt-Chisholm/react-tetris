e9ac14ddfee45d9296ce808befcd850e6f16433b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-28 11:17:13 -0700

    styled tetris

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
 export default function Cell({ type }) {
-  return <div>Cell</div>;
+  return (
+    <div>
+      <StyledCell type={type} color={TETROMINOS[type].color} />
+    </div>
+  );
 }
