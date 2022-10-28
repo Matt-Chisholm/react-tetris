b3e9ead37df7e3c33232a7f0dcfb5c2f8cb1132d
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-28 12:06:18 -0700

    tetris clean up

--- src/Hooks/useStage.js
+++ src/Hooks/useStage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createStage } from "../gameHelpers";
+import { createStage } from "../gameHelper";
 
 // Custom Hook that sets the stage state and updates the stage state
 export const useStage = (player, resetPlayer) => {
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { createStage } from "../gameHelper";
+// Styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+
+// Custom Hooks
+import { usePlayer } from "../Hooks/usePlayer";
+import { useStage } from "../Hooks/useStage";
 
 //Components
 import Stage from "./Stage";
@@ -9,10 +13,17 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 
 export default function Tetris() {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("rerender");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
           <div>
             <Display text='Score' />
