5d923dacf274f89758a8b1c3f2d62802281cb05b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-29 09:40:29 -0700

    start game and drop

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,9 +19,37 @@ export default function Tetris() {
   const [player] = usePlayer();
   const [stage, setStage] = useStage(player);
 
+  const movePlayer = (dir) => {};
+
+  const startGame = () => {
+    //Reset everything
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  };
+
   console.log("rerender");
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' on KeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
